737cea1a6c85eed7c23c546841e2d873514faacd
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-16 18:41:31 -0300

    ready to use backend create user in front end

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,36 +1,38 @@
-const router = require("express").Router();
-const User = require("../models/User");
+const router = require('express').Router();
+const User = require('../models/User');
 
-//creating user
-router.post("/", async (req, res) => {
+// creating user
+router.post('/', async(req, res)=> {
   try {
-    const { name, email, password, picture } = req.body;
+    const {name, email, password, picture} = req.body;
     console.log(req.body);
-    const user = await User.create({ name, email, password, picture });
+    const user = await User.create({name, email, password, picture});
     res.status(201).json(user);
-  } catch (error) {
+  } catch (e) {
     let msg;
-    if (error.code == 11000) {
-      msg = "User already exists";
+    if(e.code == 11000){
+      msg = "User already exists"
     } else {
-      msg = error.message;
+      msg = e.message;
     }
-    console.log(error);
-    res.status(400).json(msg);
+    console.log(e);
+    res.status(400).json(msg)
   }
-});
+})
 
-//login user
-router.post('/login', async(req,res) => {
-    try {
-        const {email, password} = req.body;
-        const user = await User.findbyCredentials(email, password);
-        user.status = 'online';
-        await user.save();
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(400).json(error.message);
-    }
-});
+// login user
 
-module.exports = router;
+router.post('/login', async(req, res)=> {
+  try {
+    const {email, password} = req.body;
+    const user = await User.findByCredentials(email, password);
+    user.status = 'online';
+    await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+      res.status(400).json(e.message)
+  }
+})
+
+
+module.exports = router
