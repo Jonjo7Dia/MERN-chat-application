db02ff08efd185d40de8defbcfee6bcb5b39c6e4
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-16 17:15:59 -0300

    sign up page state created

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Col, Row, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import bot from "../assets/bot.jpeg";
 import "./Signup.css";
+
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
-    function validateImg(){
+  //image upload state
+  const [image, setImage] = useState(null);
+  const [uploadingImg, setUploadingImg] = useState(false);
+  const [imagePreview, setImagePreview] = useState(null);
 
+  function validateImg(e) {
+    const file  = e.target.files[0];
+    if(file.size > 1048576){
+        return alert('Max file size is 1mb');
+    } else {
+        setImage(file);
+        setImagePreview(URL.createObjectURL(file));
     }
+}
+async function uploadImage(){
+    const data = new FormData();
+    data.append('file', image);
+    data.append('upload_preset', 'jonjo7dia');
+    try {
+        setUploadingImg(true);
+        let res = await fetch('http://api.cloudinary.com/v1_1/dvtxhuiks/image/upload', {
+            method: 'POST',
+            body: data,
+        });
+        const urlData = await res.json();
+        setUploadingImg(false);
+        return urlData.url
+    } catch(error){
+        setUploadingImg(false);
+        console.log(error);
+    }
+}
+async function signupHandler(e){
+    e.preventDefault();
+    if(!image) return alert('Please upload your profile picture');
+    const url = await uploadImage(image);
+    console.log(url);
+    //sign up the user
+}
   return (
     <Container>
       <Row>
@@ -18,10 +58,10 @@ export default function Signup() {
             "d-flex align-items-center justify-content-center flex-direction-column"
           }
         >
-          <Form style={{ width: "80%", maxWidth: 500 }}>
+          <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={signupHandler}>
             <h1 className={"text-center"}>Create Account</h1>
             <div className={"signup-profile-pic__container"}>
-              <img src={bot} alt="" className={"signup-profile-pic"} />
+              <img src={imagePreview || bot} alt="" className={"signup-profile-pic"} />
               <label htmlFor="image-upload" className={"image-upload-label"}>
                 <i className={"fas fa-plus-circle add-picture-icon"}></i>
               </label>
@@ -35,12 +75,24 @@ export default function Signup() {
             </div>
             <Form.Group className="mb-3 " controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Your Name" />
+              <Form.Control
+                type="text"
+                placeholder="Your Name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+
+              />
             </Form.Group>
 
             <Form.Group className="mb-3 " controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -48,10 +100,15 @@ export default function Signup() {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Submit
+              {uploadingImg ? 'Creating Account.... ' : 'Create account'}
             </Button>
 
             <div className={"py-4"}>
