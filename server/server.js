7f9432859cf07da090fdfd4385aa177ed994050f
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-16 17:50:32 -0300

    connection to mongodb created

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
+require('./connection');
 
 const server = require('http').createServer(app);
 const PORT = 5001;
@@ -21,3 +22,4 @@ const io = require('socket.io')(server, {
 server.listen(PORT, ()=>{
     console.log('listening to port', PORT);
 });
+
