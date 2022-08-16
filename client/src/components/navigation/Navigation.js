c591d47eb6a1fc77736e29227637f02caa433e68
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-16 15:11:00 -0300

    assets added

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {Nav, Navbar, Container, Button, NavDropdown} from 'react-bootstrap';
+import { Nav, Navbar, Container, Button, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 export default function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
@@ -7,9 +8,13 @@ export default function Navigation() {
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto">
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/chat">
+              <Nav.Link>Chat</Nav.Link>
+            </LinkContainer>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
