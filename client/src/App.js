5292e3f49e9bc18afc38dc76ab66fec1d62303e6
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-17 12:39:35 -0300

    fix logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import Navigation from "./components/navigation/Navigation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Chat from './pages/Chat';
+import { useSelector } from "react-redux";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
 import "./App.css";
 
 function App() {
+  const user = useSelector((state) => state.user);
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={<Chat />} />
+        {!user && (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </>
+        )}
+
+        {user && <Route path="/chat" element={<Chat />} />}
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,36 +1,59 @@
 import React from "react";
 import { Nav, Navbar, Container, Button, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import {useSelector} from 'react-redux'
+import {useLogoutUserMutation} from '../../services/appApi'
 import logo from "../../assets/logo.png";
 export default function Navigation() {
+  const user = useSelector((state) => state.user);
+  const [logoutUser] = useLogoutUserMutation();
+  async function logoutHandler(e){
+    e.preventDefault();
+    await logoutUser;
+    //
+    window.location.replace('/');
+  }
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <LinkContainer to="/">
           <Navbar.Brand>
-              <img src={logo} alt="" style={{width: 50, height: 50}}/>
+            <img src={logo} alt="" style={{ width: 50, height: 50 }} />
           </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
+            {!user && (
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            )}
+
             <LinkContainer to="/chat">
               <Nav.Link>Chat</Nav.Link>
             </LinkContainer>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
+            {user && (
+                            <NavDropdown
+                                title={
+                                    <>
+                                        <img src={user.picture} style={{ width: 30, height: 30, marginRight: 10, objectFit: "cover", borderRadius: "50%" }} />
+                                        {user.name}
+                                    </>
+                                }
+                                id="basic-nav-dropdown"
+                            >
+                                {/* <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
+
+                                <NavDropdown.Item>
+                                    <Button variant="danger" onClick={logoutHandler} >
+                                        Logout
+                                    </Button>
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        )}
           </Nav>
         </Navbar.Collapse>
       </Container>
