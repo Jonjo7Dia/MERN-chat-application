ed84a8ff2188fde719029e76c116fbf93bda8f88
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-17 12:00:46 -0300

    appApi updated

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore';
+import store from './store';
+
+const persistedStore = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+<Provider store={store}>
+    <PersistGate loading={null} persistor={persistedStore}>
+    <App />
+
+    </PersistGate>
+</Provider>
+);
--- client/src/services/appApi.js
+++ client/src/services/appApi.js
@@ -1,43 +1,44 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-//define a service user a base URL
+// define a service user a base URL
 
 const appApi = createApi({
-    reducePath: 'appApi',
+    reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5001'
+        baseUrl: "http://localhost:5001",
     }),
-    endpoints: (builder)=>({
-        //creating the user
+
+    endpoints: (builder) => ({
+        // creating the user
         signupUser: builder.mutation({
             query: (user) => ({
-                url: '/user',
-                method: 'POST',
-                body: user,
-
-            })
-        }),
-
-        //login
-        loginUser: builder.mutation({
-            query: (user) => ({
-                url: '/user',
-                method: 'POST',
+                url: "/users",
+                method: "POST",
                 body: user,
             }),
         }),
 
-        // logout 
+        // login
+        loginUser: builder.mutation({
+            query: (user) => ({
+                url: "/users/login",
+                method: "POST",
+                body: user,
+            }),
+        }),
+
+        // logout
+
         logoutUser: builder.mutation({
             query: (payload) => ({
-                url: '/logout',
-                method: 'DELETE',
+                url: "/logout",
+                method: "DELETE",
                 body: payload,
-            })
+            }),
         }),
-    })
+    }),
 });
 
-export const {useSignUpUserMutation, useLoginUserMutation, useLogoutUserMutation} = appApi;
+export const { useSignupUserMutation, useLoginUserMutation, useLogoutUserMutation } = appApi;
 
 export default appApi;
