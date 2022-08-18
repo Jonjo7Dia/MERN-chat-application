2e509d4a4d251468e03c414e2fcc96b0f3cccbdb
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-18 15:20:09 -0300

    message implementation backend

--- server/server.js
+++ server/server.js
@@ -1,25 +1,65 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userRoutes = require('./routes/userRoutes');
-const rooms = ['general', 'tech' , 'finance', 'crypto'];
-const cors = require('cors');
+const userRoutes = require("./routes/userRoutes");
+const rooms = ["general", "tech", "finance", "crypto"];
+const cors = require("cors");
+const User = require("./models/User");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.use('/users',userRoutes);
-require('./connection');
+app.use("/users", userRoutes);
+require("./connection");
 
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 const PORT = 5001;
-const io = require('socket.io')(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        method: ['GET', 'POST'],
-    }
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+  },
 });
 
-server.listen(PORT, ()=>{
-    console.log('listening to port', PORT);
+app.get("/rooms", (req, res) => {
+  res.json(rooms);
 });
 
+async function getLastMessagesFromRoom(room) {
+  let roomMessages = await Message.aggregate([
+    { $match: { to: room } },
+    { $group: { _id: "$date", messageByDate: { push: "$$ROOT" } } },
+  ]);
+  return roomMessages;
+}
+
+//02/11/2022
+
+function sortRoomMessagesByDate(messages) {
+  return messages.sort(function (a, b) {
+    let date1 = a._id.split("/");
+    let date2 = b._id.split("/");
+    date1 = date1[2] + date1[0] + date1[1];
+    date2 = date2[2] + date2[0] + date2[1];
+    return date1 < date2 ? -1 : 1;
+  });
+}
+
+//socket connection
+
+io.on("connection", (socket) => {
+ socket.on('new-user', async ()=>{
+     const members = await User.find();
+     io.emit('new-user', members);
+ })
+
+  socket.on("join-room", async (room) => {
+    socket.join(room);
+    let roomMessages = await getLastMessagesFromRoom(room);
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    socket.emit('room-messages', roomMessages);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("listening to port", PORT);
+});
